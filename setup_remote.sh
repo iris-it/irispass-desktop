09c5ee5ee4228a206cea0dd1e777d1c4b516d437
Alexandre <alexandre.mangin@viacesi.fr>
2016-12-15 10:23:18 +0100

    Update setup_remote.sh

--- setup_remote.sh
+++ setup_remote.sh
@@ -1,26 +1,25 @@
 #!/usr/bin/env bash
 
+# clean install
 rm -rf osjs
-
 git clone https://github.com/os-js/OS.js.git osjs
 
 # install the dependencies of the project
 npm install
 
-# make the override from the project to os.js
-grunt build
-
 # no comment
 cd osjs
 
 # install the dependencies of os.js
-npm install
-npm install node-mysql bcryptjs node-rest-client
+npm install --production
+npm install node-rest-client
 git clone https://github.com/gildas-lormeau/zip.js.git vendor/zip.js
 
 # build os.js
-grunt
+node osjs build
 
+# back to override project
 cd ..
 
+# make the override from the project to os.js
 grunt build
